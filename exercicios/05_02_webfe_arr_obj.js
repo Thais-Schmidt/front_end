78e303c13442b07526281e9689918b9564fe635a
Thais Schmidt <thaisachmidtdasilva@gmail.com>
2023-10-03 00:07:30 -0300

    exercicios finalizados

--- exercicios/05_02_webfe_arr_obj.js
+++ exercicios/05_02_webfe_arr_obj.js
@@ -1,1 +1,20 @@
 // Criar uma função utilizando for ou while que RETORNE um array com a seguinte contagem: 0 3 6 9 12 15 18.
+
+const retornando = () => {
+
+    const array = [];
+    let i = 0;
+
+    while(i <= 18){
+        
+        array.push(i)
+        i += 3;
+        
+    }
+
+    return array;
+
+}
+
+const retorno = retornando();
+console.log(retorno)
--- exercicios/05_03_webfe_arr_obj.js
+++ exercicios/05_03_webfe_arr_obj.js
@@ -6,41 +6,34 @@
 // - Entre 3 e 10 anos: aumento de 12.5%
 // - 10 anos ou mais: aumento de 20%
 
+const nome = "Thais Schmidt";
+const salario = 1900.00;
+const anos = 11;
 
-const aumentoSal = (a,b,c) => {
+const aumentoSal = (nome, salario, anos) => {
 
-    let salario = a;
-    let nomeFuncionario = b;
-    let anosTrabalhados = c;
+    let aumentoSalario;
 
-    if (c <= 3){
+    if (anos <= 3){
         
-        var porcentagem = a * 0.03;
-        var aumento = a + porcentagem;
+        aumentoSalario = salario * 1.03;
         
-        return console.log(`O funcionario ${b} tera um reajuste de aumento salarial de 3%, ficando assim, um salario de ${aumento} `);
+        return `O funcionario ${nome} tera um reajuste de aumento salarial de 3%, ficando assim, um salario de ${aumentoSalario}.`;
 
-    } else if(c >= 3 || a <=10) {
+    } else if(anos <=10) {
 
-        var porcentagem = a * 0.125;
-        var aumento = a + porcentagem;
+        aumentoSalario = salario * 1.125;
 
-        return console.log(`O funcionario ${b} tera um reajuste de aumento salarial de 12.5%, ficando assim, um salrio de ${aumento} `);
+        return `O funcionario ${nome} tera um reajuste de aumento salarial de 12.5%, ficando assim, um salario de ${aumentoSalario}.`;
 
-    } else if(c > 10) {
+    } else {
 
-        var porcentagem = a * 0.2;
-        var aumento = a + porcentagem;
+        aumentoSalario = salario * 1.2;
 
-        return console.log(`O funcionario ${b} tera um reajuste de aumento salarial de 20%, ficando assim, um salrio de ${aumento} `);
+        return `O funcionario ${nome} tera um reajuste de aumento salarial de 20%, ficando assim, um salario de ${aumentoSalario}.`;
     }
         
 }
 
-const funcionarioExp = [
-    {nomeFuncionario: `Joaquin Silva`, salario: `1500`, anosTrab: 3},
-    {nomeFuncionario: `Eduardo Machado`, salario: `2000`, anosTrab: 5},
-    {nomeFuncionario: `Monica Suzano`, salario: `3500`, anosTrab: 11},
-]
-
-const funcA = funcionarioExp.nomeFuncionario;
+const func = aumentoSal(nome, salario, anos)
+console.log(func)
