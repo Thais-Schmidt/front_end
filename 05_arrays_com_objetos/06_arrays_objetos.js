acae5cf0d2fc40ddbbac9d3059906fd3c169bf84
Thais Schmidt <thaisachmidtdasilva@gmail.com>
2023-10-04 20:53:56 -0300

    atualizando pasta de front-end

--- 05_arrays_com_objetos/06_arrays_objetos.js
+++ 05_arrays_com_objetos/06_arrays_objetos.js
@@ -5,7 +5,7 @@ console.log(hoje);
 
 console.log(`------- Date - Formato com localidade -------`);
 
-console.log(hoje.toLocaleDateString().hoje.toLocaleDateString());
+console.log(hoje.toLocaleDateString(), hoje.toLocaleDateString());
 
 console.log(hoje.toLocaleDateString('pt-br'));
 
